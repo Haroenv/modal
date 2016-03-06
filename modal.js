1a0cc5305525eedc3e6320ca2a743b366dbbf884
Haroen Viaene <haroen.viaene@student.odisee.be>
2016-03-06 14:14:04 +0100

    convert to UMD

--- modal.js
+++ modal.js
@@ -7,39 +7,68 @@
  * @author Haroen Viaene <hello@haroen.me>
  * @version 0.1
  */
-var modal = function(text,agreeCallback,disagreeCallback){
-	var modal = document.createElement('div');
-	var cancel = document.createElement('button');
-	var okay = document.createElement('button');
-	var buttons = document.createElement('div');
-	var content = document.createElement('p');
+(function (root, factory) {
+  'use strict';
 
-	content.appendChild(document.createTextNode(text));
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], function () {
+      return (root.modal = factory());
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like enviroments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals
+    root.modal = factory();
+  }
+})(this, function () {
+  // UMD Definition above, do not remove this line
 
-	modal.appendChild(content);
-	modal.appendChild(buttons);
-	buttons.appendChild(cancel);
-	buttons.appendChild(okay);
+  // To get to know more about the Universal Module Definition
+  // visit: https://github.com/umdjs/umd
 
-	modal.className += 'modal';
-	content.className += 'modal--content';
-	cancel.className += 'modal--cancel';
-	okay.className += 'modal--okay';
+  'use strict';
 
-	cancel.innerHTML = 'Cancel';
-	okay.innerHTML = 'OK';
+  var modal = function(text,agreeCallback,disagreeCallback){
+    var div = document.createElement('div');
+    var cancel = document.createElement('button');
+    var okay = document.createElement('button');
+    var buttons = document.createElement('div');
+    var content = document.createElement('p');
 
-	document.body.insertBefore(modal, document.body.firstChild);
+    content.appendChild(document.createTextNode(text));
 
-	cancel.focus();
+    div.appendChild(content);
+    div.appendChild(buttons);
+    buttons.appendChild(cancel);
+    buttons.appendChild(okay);
 
-	modal.querySelector('.modal--okay').addEventListener('click',function(){
-		document.body.removeChild(modal);
-		agreeCallback();
-	});
+    div.className = 'modal';
+    content.className = 'modal--content';
+    cancel.className = 'modal--cancel';
+    okay.className = 'modal--okay';
 
-	modal.querySelector('.modal--cancel').addEventListener('click',function(){
-		document.body.removeChild(modal);
-		disagreeCallback();
-	});
-};
+    cancel.innerHTML = 'Cancel';
+    okay.innerHTML = 'OK';
+
+    document.body.insertBefore(div, document.body.firstChild);
+
+    cancel.focus();
+
+    div.querySelector('.modal--okay').addEventListener('click',function(){
+        document.body.removeChild(div);
+        agreeCallback();
+    });
+
+    div.querySelector('.modal--cancel').addEventListener('click',function(){
+        document.body.removeChild(div);
+        disagreeCallback();
+    });
+  };
+
+  return modal;
+
+});
